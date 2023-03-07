2c467ba3b6e0a6b92429695ef646c8054c7914dc
Daniel Wang <e1010625221@gmail.com>
2023-03-07 23:24:49 +0800

    commit

--- server/socketio_hanlder.py
+++
@@ -1,7 +0,0 @@
-from server import app
-
-@app.sio.on('message')
-async def handle_join(sid, data):
-    print(f"Received message from {sid}: {data}")
-    await app.sio.emit("message", f"Hello, {sid}!")
-
